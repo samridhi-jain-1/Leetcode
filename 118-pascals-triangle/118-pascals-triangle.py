b95e00e9415edaa80bd4b039e8eb01f89b5df967
samridhi-jain-1 <85945015+samridhi-jain-1@users.noreply.github.com>
2022-07-07 12:15:12 +0530

    Time: 52 ms (36.09%), Space: 13.9 MB (66.49%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,11 +1,11 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
-        ans=[]
-        for i in range(0,n):
-            r=[None]*(i+1)
-            r[0]=r[-1]=1
-            for j in range(1,i):
-                r[j]=ans[i-1][j-1]+ans[i-1][j]
-            ans.append(r)    
-        return ans
+        res=[[1]]
+        for i in range(1,n):
+            t=[0]+res[-1]+[0]
+            r=[]
+            for j in range(i+1):
+                r.append(t[j]+t[j+1])
+            res.append(r) 
+        return res  
         
