9e6c02ee554535050a0e73c4c4e77062fa1b438b
samridhi-jain-1 <85945015+samridhi-jain-1@users.noreply.github.com>
2022-07-13 14:57:12 +0530

    Time: 48 ms (84.93%), Space: 14 MB (24.65%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,34 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # n=len(nums)    
-        # i=n-2
-        # while(i>=0 and nums[i]>=nums[i+1]):
-        #     i-=1  
-        # if i==-1:
-        #     nums=nums[::-1]
-        # else:
-        #     for k in range(n-1,i,-1):
-        #         if(nums[i]<nums[k]):
-        #             nums[i],nums[k]=nums[k],nums[i]
-        #     nums[i+1:n]=nums[n-1:i:-1]  
-        # print(nums)   
-        n = len(nums)
-        index_1, index_2 = 0, 0
-        for i in range(n-2, 0, -1):
-            if nums[i] < nums[i+1]:
-                index_1 = i
-                break
-
-        for i in range(n-1, 0, -1):
-            if nums[i] > nums[index_1]:
-                index_2 = i
-                break
-        
-        nums[index_1], nums[index_2] = nums[index_2], nums[index_1]
-        if index_1 == 0 and index_2 == 0:
-            nums[:] = nums[::-1]
+        n=len(nums)    
+        i=n-2
+        while(i>=0 and nums[i]>=nums[i+1]):
+            i-=1  
+        print(i)    
+        if i==-1:
+            nums[::]=nums[::-1]
         else:
-            nums[index_1+1: ] = nums[:index_1:-1]
+            for k in range(n-1,i,-1):
+                if(nums[i]<nums[k]):
+                    print(k)
+                    nums[i],nums[k]=nums[k],nums[i]
+                    break
+            print(nums)        
+            nums[i+1:n]=nums[n-1:i:-1]  
+           
+
+
         
         
